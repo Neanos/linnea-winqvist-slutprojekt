822c4b2fd01676e052b3e9b63e7301dfe46b05a0
Linnea Winqvist <winqvist.linnea@elev.ga.ntig.se>
2022-05-18 13:04:03 +0200

    18/5

---
+++ linnea-winqvist-slutprojekt/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+string userName = "";
+
+while (userName != "Linnea"){
+    Console.WriteLine("Skriv in ditt lösenord");
+    userName = Console.ReadLine();
+    
+    Console.WriteLine("Välkomen in");
+    Console.ReadLine();
+
+}
+
